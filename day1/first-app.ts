ff60dc5a726d37027d5097ba9a5af542dc0923f2
Sabbha20 <sabbha.power1@gmail.com>
2026-07-09 19:00:48 +0530

    day1: object type

--- day1/first-app.ts
+++ day1/first-app.ts
@@ -11,3 +11,21 @@ console.log(`User ID is "${usrID}"`);
 // changing the value of usrID to a number
 usrID = 1001;
 console.log(`User ID is now "${usrID}"`);
+
+// Object type
+let user: {
+    name: string;
+    age: number;
+    isValid: boolean;
+    id: string | number;
+};
+
+user = {
+    name: "Sabbha",
+    age: 31,
+    isValid: true,
+    id: "User 1001"
+};
+
+console.log(`User details: Name - ${user.name}, Age - ${user.age}, Valid - ${user.isValid}, ID - ${user.id}`);
+
