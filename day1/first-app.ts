a76ae0fae6fd4ab1ea579631d45d7bbb364c3935
Sabbha20 <sabbha.power1@gmail.com>
2026-07-09 16:16:05 +0530

    day1: using dual type in single variable

--- day1/first-app.ts
+++ day1/first-app.ts
@@ -3,3 +3,11 @@ let age: number = 31;
 let mybool: boolean = true;
 
 console.log(`My name is ${usrname} and I am ${age} years old. It is ${mybool} that I am learning TypeScript.`);
+
+// using dual type in single variable
+let usrID: string | number = "User 1001";
+console.log(`User ID is "${usrID}"`);
+
+// changing the value of usrID to a number
+usrID = 1001;
+console.log(`User ID is now "${usrID}"`);
